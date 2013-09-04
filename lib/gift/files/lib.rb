2e64b42263edd69abbd34b9783c0e4c6a5cc2c33
var_j <jesly.varghese@gmail.com>
2013-09-05 02:47:35 +0530

    added location param to clone method

--- lib/gift/files/lib.rb
+++ lib/gift/files/lib.rb
@@ -7,8 +7,8 @@ module Git
       `git diff --name-only #{commit_one} #{commit_two}`.split '\n'
     end
 
-    def clone(repository_url)
-      `git clone #{repository_url}`
+    def clone(repository_url, location)
+      `git clone #{repository_url} #{location}`
     end
   end
 end
