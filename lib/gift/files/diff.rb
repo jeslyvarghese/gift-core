f29211dac39bbb6a22a2631b1be7df1d96128bdb
var_j <jesly.varghese@gmail.com>
2013-09-05 02:48:30 +0530

    files diffed between commits get shown

--- lib/gift/files/diff.rb
+++ lib/gift/files/diff.rb
@@ -4,4 +4,22 @@
  
 class Diff
   attr_accessor :old_commit, :new_commit, :files
+  def intialize(oldc, newc)
+    @old_commit = oldc
+    @new_commit = newc
+    @files = self.files_diffed
+  end
+
+  def files_diffed
+    Git::Lib.new.diff @old_commit, @new_commit    
+  end
+
+  def old_commit=(old_commit)
+    @old_commit = old_commit
+    @files = self.files_diffed
+  end
+  def new_commit=(new_commit)
+    @new_commit = new_commit
+    @files+ = self.files_diffed
+  end
 end
