b1e6927c4744f6fa36792200ba9d7d43102b8026
var_j <jesly.varghese@gmail.com>
2013-11-17 20:31:11 +0530

    edited to reflect changes in Git::Lib.delta syntax

--- lib/gift/files/lib.rb
+++ lib/gift/files/lib.rb
@@ -3,8 +3,8 @@ module Git
     def initialize
     end
 
-    def delta(commit_one, commit_two)
-      `git diff --name-only #{commit_one} #{commit_two}`.split '\n'
+    def delta(commit_one, commit_two, git_folder_location)
+      `git --git-dir='#{git_folder_location}/.git' diff #{commit_one} #{commit_two}  --name-only`.split "\n"
     end
 
     def clone(repository_url, location)
