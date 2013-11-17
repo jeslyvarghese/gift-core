e928c97ad8b825b4228468bcca5c6c2b8174980a
var_j <jesly.varghese@gmail.com>
2013-11-17 20:28:53 +0530

    added syntax changed on Git::Lib.diff

--- lib/gift/files/commit.rb
+++ lib/gift/files/commit.rb
@@ -4,20 +4,24 @@ require_relative 'lib.rb'
 require_relative 'diff.rb'
 
 class Commit
-  attr_accessor :id
+  attr_accessor :id, :git_location
+  def initialize(id, git_location)
+    @id = id
+    @git_location = git_location
+  end
   
   def diff(other_commit)
     #find the difference happened between
     #self and other_commit.
     # commit1 diff commit2 :=> Diff instance
-    diff = Diff.new
+    diff = Diff.new @id , other_commit.id, @git_location
     
     diff.old_commit  = @id
     diff.new_commit  = other_commit.id
 
     git_lib = Git::Lib.new
     
-    deltas = git_lib.delta @id, other_commit.id
+    deltas = git_lib.delta @id, other_commit.id, @git_location
     diff.files = deltas
     diff
   end
