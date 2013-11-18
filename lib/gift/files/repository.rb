540d2cf780d3e8d970c8a08b1bd433c519787dc6
var_j <jesly.varghese@gmail.com>
2013-11-19 01:00:16 +0530

    added function to set remote on git repo

--- lib/gift/files/repository.rb
+++ lib/gift/files/repository.rb
@@ -1,7 +1,7 @@
 require_relative 'lib.rb'
 
 class Repository
-  attr_accessor :name, :url, :commits
+  attr_accessor :name, :url, :commits, :remote, :location, :last_commit
   
   def initialize(url)
   	@url = url
@@ -9,5 +9,19 @@ class Repository
 
   def clone_to(location)
     Git::Lib.new.clone @url, location 
+  end
+  def remote=(remote_name)
+    @remote = remote_name
+    Git::Lib.new.remote_add @remote, @url, @location
+  end
+  
+  def pull(branch)
+  	@last_commit = Commit.new Commit.last_commit(@location), @location
+  	Git::Lib.new.pull @remote, branch
+  end
+
+  def chages
+  	latest_commit = Commit.new Commit.last_commit(@location), @location
+  	@last_commit - latest_commit
   end
 end
